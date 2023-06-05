36af6b1583c445821544544060551a31b5aba8b5
Roger Marques <marquesroger226@gmail.com>
2023-06-04 22:27:51 -0300

    fix: problem accessing localStorage during build

--- capputeeno/src/hooks/useLocalStorage.tsx
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -3,49 +3,43 @@ import { Product } from "@/data/Promises";
 import { useCallback, useContext, useEffect } from "react";
 
 export default function useLocalStorage(key: string) {
-    const {cart, setCart} = useContext(CheckoutContext)
-    const storage = localStorage.getItem(key)
+  const { cart, setCart } = useContext(CheckoutContext);
+  const storage =
+    typeof window !== 'undefined' ? localStorage.getItem(key) : null;
 
-    // const getLocalStorage = (): Product[] | void => {
-
-    //     if (storage !== null) {
-    //         return JSON.parse(storage)
-    //     }
-
-    //     return console.log('Informe uma chave válida');
-    // }
-
-    const getLocalStorage = useCallback(() => {
-        if (storage !== null) {
-            return JSON.parse(storage)
-        }
-
-        return console.log('Informe uma chave válida');
-    }, [storage])
-    
-    useEffect(() => {
-        const cartItems = getLocalStorage()
-
-        if (cartItems) {
-            return setCart(cartItems)
-        }
-        
-    }, [setCart, getLocalStorage])
-
-    const setLocalStorage = (value: Product) => {
-        const cart = getLocalStorage()
-        if (storage && cart) {
-            const newCart = [...cart, value]
-            setCart(newCart)
-            return localStorage.setItem(key, JSON.stringify(newCart))
-        }
-        setCart([value])
-        return localStorage.setItem(key, JSON.stringify([value]))
+  const getLocalStorage = useCallback(() => {
+    if (storage !== null) {
+      return JSON.parse(storage);
     }
 
-    return {
-        cart,
-        setLocalStorage
-    }
+    return console.log('Informe uma chave válida');
+  }, [storage]);
 
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const cartItems = getLocalStorage();
+
+    if (cartItems) {
+      setCart(cartItems);
+    }
+  }, [setCart, getLocalStorage]);
+
+  const setLocalStorage = (value: Product) => {
+    if (typeof window === 'undefined') return;
+
+    const cart = getLocalStorage();
+    if (storage && cart) {
+      const newCart = [...cart, value];
+      setCart(newCart);
+      return localStorage.setItem(key, JSON.stringify(newCart));
+    }
+    setCart([value]);
+    return localStorage.setItem(key, JSON.stringify([value]));
+  };
+
+  return {
+    cart,
+    setLocalStorage
+  };
 }
